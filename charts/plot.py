c7bdfd7275fd3a4435ec80f6ad2728e2745ea343
Arnout <arnoutaertgeerts@gmail.com>
2015-05-22 12:31:03 +0200

    Added tab and window show options

--- charts/plot.py
+++ charts/plot.py
@@ -2,9 +2,11 @@ __author__ = 'Arnout Aertgeerts'
 __version__ = '0.0.1'
 
 from string import Template
+
 import os
 import json
 import shutil
+import webbrowser
 
 package_directory = os.path.dirname(os.path.abspath(__file__))
 
@@ -14,7 +16,7 @@ class MyTemplate(Template):
     idpattern = r'[a-z][_a-z0-9]*'
 
 
-def plot(series, options, height=400, save=False, stock=False, inline=True):
+def plot(series, options, height=400, save=False, stock=False, show='tab'):
     """
     Make a highchart plot with all data embedded in the HTML
     :param series: The necessary data
@@ -22,7 +24,11 @@ def plot(series, options, height=400, save=False, stock=False, inline=True):
     :param height: Chart height
     :param save: Specify a filename to save the HTML file if wanted.
     :param stock: Set to False to use Highcharts instead of highstock
-    :param inline: Set to False to not show the chart inline
+    :param show: Determines how the chart is shown. Can be one of the following options:
+        - 'tab': Show the chart in a new tab of the default browser
+        - 'window': Show the chart in a new window of the default browser
+        - 'inline': Show the chart inline (only works in IPython notebook)
+        - False: Do not show the chart
     :return:
     """
 
@@ -43,18 +49,44 @@ def plot(series, options, height=400, save=False, stock=False, inline=True):
     if save:
         with open(save, "w") as text_file:
             text_file.write(string)
+    else:
+        if show != 'inline':
+            save = 'index.html'
+            with open(save, "w") as text_file:
+                text_file.write(string)
 
-    if inline:
+    if show == 'inline':
         from IPython.display import HTML
         return HTML(string)
 
-    if not save and not inline:
-        print 'You should either save the file or show it inline in an IPython notebook. Both options are set to False.'
+    elif show == 'tab':
+        webbrowser.open_new_tab(save)
+
+    elif show == 'window':
+        webbrowser.open_new(save)
+
+    else:
+        if save:
+            print 'Chart saved to %s', save
 
 
-def plotasync(series, options, height=400, name="chart", stock=True, inline=True):
+def plotasync(series, options, height=400, name="chart", stock=True, show='tab'):
 
     # Set the display property default to false for an asynchronous plot
+    """
+
+    :param series:
+    :param options:
+    :param height:
+    :param name:
+    :param stock:
+    :param show: Determines how the chart is shown. Can be one of the following options:
+        - 'tab': Show the chart in a new tab of the default browser
+        - 'window': Show the chart in a new window of the default browser
+        - 'inline': Show the chart inline (only works in IPython notebook)
+        - False: Do not show the chart
+    :return:
+    """
     for serie in series:
         if 'display' not in serie:
             serie['display'] = False
@@ -92,9 +124,17 @@ def plotasync(series, options, height=400, name="chart", stock=True, inline=True
             height=str(height) + "px"
         )
 
-    with open(os.path.join(name, 'index.html'), "w") as html_file:
+    html_path = os.path.join(name, 'index.html')
+    with open(html_path, "w") as html_file:
         html_file.write(html)
 
-    if inline:
+    if show == 'inline':
         from IPython.display import HTML
         return HTML(string)
+
+    elif show == 'tab':
+        webbrowser.open_new_tab(html_path)
+
+    elif show == 'window':
+        webbrowser.open_new(html_path)
+
