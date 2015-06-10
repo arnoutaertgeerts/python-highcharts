63e88221b83b40e77404b46797ba8c095946faa8
Arnout <arnoutaertgeerts@gmail.com>
2015-06-10 15:37:45 +0200

    Firs steps for async conversion

--- view/src/javascript/async.js
+++ view/src/javascript/async.js
@@ -1,236 +1,194 @@
 
-//inject:js
-require([
+requirejs([
+    'jquery',
+    'selectize',
+    'jsoneditor',
     'highstock',
-    'angular',
-    'underscore',
-    'animate',
-    'aria',
-    'material',
-    'export',
-    'accordion',
-    'select'
-], function () {
+    'export'
+], function($, selectize, JSONEditor) {
 
-    //Count the number of apps / charts on the page
-    if (window.counter == undefined) {
-        window.counter = 0;
-    } else {
-        window.counter++;
+    function guid() {
+        function s4() {
+            return Math.floor((1 + Math.random()) * 0x10000)
+                .toString(16)
+                .substring(1);
+        }
+        return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+            s4() + '-' + s4() + s4() + s4();
     }
 
-    //Create different containers for the apps and charts
-    var chartContainer = document.getElementById("container");
-    chartContainer.id = "chart" + window.counter.toString();
+    var id = guid();
+    plot(id);
 
-    var appContainer = document.getElementById("appid");
-    appContainer.id = "app" + window.counter.toString();
+    function plot(id) {
+        var options = {};
+        //replace-options
+        var useHighStock = false;
+        //replace-highstock
+        var save = 'app/chart.svg';
+        //replace-save
+        var url = 'http://127.0.0.1:37759';
+        //replace-url
 
-    //The angular app
-    var app = angular.module('myApp', [
-        "ngMaterial",
-        "vAccordion",
-        "ng.jsoneditor",
-        "isteven-multi-select"
-    ]);
+        //Create different containers for the charts
+        var chartContainer = document.getElementById("chart-container");
+        chartContainer.id = "chart-container" + id;
 
-    app.controller('MyController', [
-        '$scope',
-        '$http',
-        '$interval',
-        '$q', function ($scope, $http, $interval, $q) {
-            var vm = this;
+        var selector = $("#variable-selector");
+        selector.attr('id', "variable-selector" + id);
 
-            var options = $$options;
-            var useHighStock = $$highstock;
-            var path = $$path;
-            var live = $$live;
+        var settings = $("#settings-collapse");
+        settings.attr('id', "settings-collapse" + id);
 
-            var ChartType = useHighStock ? Highcharts.StockChart : Highcharts.Chart;
+        var button = $("#settings-button");
+        button.attr('id', "settings-button" + id);
 
-            var displayed = [];
+        var saveButton = $("#save-settings");
+        saveButton.attr('id', "save-settings" + id);
 
-            $http.get(path + '/keys.json').success(function (data) {
-                var sync;
+        var updateButton = $("#update");
+        updateButton.attr('id', "update" + id);
 
-                vm.keys = data;
+        var liveToggle = $("#live-toggle");
+        liveToggle.attr('id', "live-toggle" + id);
 
-                vm.selected = {};
-                vm.selected.keys = [];
+        // create the editor
+        var editorContainer = document.getElementById("jsoneditor");
+        editorContainer.id = "jsoneditor" + id;
+        var editor = new JSONEditor(editorContainer);
 
-                vm.showLiveToggle = live;
-                vm.live = live;
+        button.on('click', showSettings);
 
-                vm.options = null;
-                vm.setOptions = setOptions;
+        saveButton.on('click', function () {
+            var newOptions = editor.get();
+            //Prevent export from breaking
+            delete newOptions.exporting;
+            setOptions(newOptions);
+            settings.collapse('hide');
+        });
 
-                $scope.$watch('vm.selected.keys', updateSeries, true);
-                $scope.$watch('vm.live', toggleSync);
+        function showSettings() {
+            settings.collapse('toggle');
+        }
 
-                angular.element(appContainer).on('destroy', function () {
-                    console.log('destroyed!');
+        //Choose a chart type
+        var ChartType = useHighStock ? Highcharts.StockChart : Highcharts.Chart;
+
+        //Default highchart colors
+        var colors = ['#7cb5ec', '#434348', '#90ed7d', '#f7a35c', '#8085e9',
+            '#f15c80', '#e4d354', '#2b908f', '#f45b5b', '#91e8e1'];
+
+        var displayed = [];
+        var keys = [];
+
+        //Set initial chart options
+        var chartOptions;
+        if (typeof options.chart === "undefined") {
+            chartOptions = {renderTo: chartContainer.id};
+        } else {
+            chartOptions = $.extend(options["chart"], {renderTo: chartContainer.id});
+        }
+
+        //Initial rendered series
+        var renderedSeries = [];
+        options = angular.extend(options, {chart: chartOptions}, {series: renderedSeries});
+        var chart = new ChartType(options);
+
+        selector.selectize({
+            plugins: ['remove_button', 'restore_on_backspace'],
+            delimiter: ',',
+            options: [],
+            items: [],
+            onItemAdd: function (key) {
+                console.log('series added');
+                addSeries(key);
+                newChart(chart.options, renderedSeries);
+            },
+            onItemRemove: function (key) {
+                console.log('series removed');
+                deleteSeries(key);
+                newChart(chart.options, renderedSeries);
+            }
+        });
+
+        //Check for new keys
+        function update() {
+            $.get(path + '/keys.json').done(function (data) {
+                //Get the new keys
+                var oldKeys = keys.map(function (element) {
+                    return element.name
                 });
 
-                vm.update = update;
+                //Get the new key objects
+                var newKeyObjects = data.filter(function (element) {
+                    return oldKeys.indexOf(element.name) == -1
+                });
 
-                if (typeof options.chart === "undefined") {
-                    var chartOptions = {renderTo: chartContainer.id};
-                } else {
-                    var chartOptions = angular.extend(options["chart"], {renderTo: chartContainer.id});
-                }
-
-                //Initial rendered series
-                var renderedSeries = [];
-                options = angular.extend(options, {chart: chartOptions}, {series: renderedSeries});
-                var chart = new ChartType(options);
-
-                //JSONEditor object
-                vm.obj = {data: chart.options, options: { mode: 'tree' }};
-
-                //Default highchart colors
-                var colors = {};
-                colors.available = ['#7cb5ec', '#434348', '#90ed7d', '#f7a35c', '#8085e9',
-                    '#f15c80', '#e4d354', '#2b908f', '#f45b5b', '#91e8e1'];
-                colors.used = [];
-
-                function toggleSync() {
-                    vm.live ? startSync() : stopSync();
-                }
-
-                function startSync() {
-                    stopSync();
-                    sync = $interval(update, 1000)
-                }
-
-                function stopSync() {
-                    $interval.cancel(sync)
-                }
-
-                function setOptions(options) {
-                    newChart(angular.extend(chart.options, options), renderedSeries);
-                    vm.accordion.toggle(0);
-                }
-
-                /**
-                 * Create filter function for a query string
-                 */
-                function filter(query) {
-                    var lowercaseQuery = angular.lowercase(query);
-
-                    return function filterFn(key) {
-                        return (key.toLowerCase().indexOf(lowercaseQuery) != -1);
-                    };
-
-                }
-
-                function search(query) {
-                    if (!query) {
-                        return [];
-                    }
-                    var availableKeys = _.difference(vm.keys, vm.selected.keys);
-                    var results = availableKeys.filter(filter(query));
-                    return results.length >= 1 ? results : availableKeys;
-                }
-
-                //Update the series
-                function updateSeries(newValue, oldValue) {
-                    newValue = newValue.map(function (element) {
-                        return element.name
-                    });
-
-                    oldValue = oldValue.map(function (element) {
-                        return element.name
-                    });
-
-                    if (newValue.length > oldValue.length) {
-                        //Fetch one key at a time in the asynchronous case
-                        var newKeys = _.difference(newValue, oldValue);
-                        var promises = [];
-                        angular.forEach(newKeys, function (newKey) {
-                            promises.push($http.get(path + '/' + newKey + '.json').success(function (data) {
-                                addSeries(data);
-                            }))
-                        });
-
-                        $q.all(promises).then(function () {
-                            newChart(chart.options, renderedSeries);
-                        });
-
-
-                    } else if (newValue.length < oldValue.length) {
-                        var removedKey = _.difference(oldValue, newValue)[0];
-                        deleteSeries(removedKey);
-                        newChart(chart.options, renderedSeries)
-                    }
-                }
-
-                //Check for new keys
-                function update() {
-                    $http.get(path + '/keys.json').success(function (data) {
-
-                        //Get the new keys
-                        var oldKeys = vm.keys.map(function(element) {return element.name});
-
-                        //Get the new key objects
-                        var newKeyObjects = _.filter(data, function(element) {
-                            return oldKeys.indexOf(element.name) == -1
-                        });
-
-                        //Add the new object to the scope
-                        vm.keys.push.apply(vm.keys, newKeyObjects);
-                        //vm.selected.keys.push.apply(vm.selected.keys, newKeyObjects)
-
-                    })
-                }
-
-                function findSeries(series, key) {
-                    var index = {};
-
-                    var arrayLength = series.length;
-                    for (var i = 0; i < arrayLength; i++) {
-                        if (series[i].name == key) {
-                            index = i
-                        }
-                    }
-                    return index
-                }
-
-                function newChart(options, series) {
-                    //Disable animation
-                    newOptions = angular.extend(options, {series: series});
-                    newOptions.plotOptions['series'] = {animation: false};
-
-                    //Get zoom
-                    var xExtremes = chart.xAxis[0].getExtremes();
-
-                    //Re-plot the chart
-                    chart.destroy();
-                    chart = new ChartType(newOptions);
-
-                    //Reset the zoom
-                    chart.xAxis[0].setExtremes(xExtremes.min, xExtremes.max, false, false);
-
-                    //Re-draw chart
-                    chart.redraw();
-                }
-
-                function addSeries(newSeries) {
-                    renderedSeries.push(angular.extend(newSeries, {color: colors.available[0]}));
-                    colors.used.push(colors.available.splice(0, 1)[0]);
-                }
-
-                function deleteSeries(name) {
-                    var index = findSeries(renderedSeries, name);
-                    var deletedColor = renderedSeries.splice(index, 1).color;
-                    var colorIndex = colors.used.indexOf(deletedColor);
-                    colors.available.push(colors.used.splice(colorIndex, 1)[0]);
-                }
-
-            }).error(function (error) {
-                console.log(error);
+                //Set the keys equal to all keys and update the selector
+                keys = data;
+                selector.clearOptions();
+                selector.addOption(keys);
+                selector.refreshOptions();
             })
 
-        }]);
+        }
 
-    angular.bootstrap(appContainer, ['myApp']);
+        $.get(path + '/keys.json').done(function (data) {
+            keys = data;
+
+        }).error(function (error) {
+            console.log(error);
+        });
+
+        function setOptions(options) {
+            newChart(options, renderedSeries);
+        }
+
+        function findSeries(series, key) {
+            return series.findIndex(function (obj) {
+                return obj.name == key;
+            })
+        }
+
+        function newChart(options, series) {
+            //Disable animation
+            var newOptions = $.extend(options, {series: series});
+            newOptions.plotOptions['series'] = {animation: false};
+
+            //Get zoom
+            var xExtremes = chart.xAxis[0].getExtremes();
+
+            //Re-plot the chart
+            chart.destroy();
+            chart = new ChartType(newOptions);
+
+            //Reset the zoom
+            chart.xAxis[0].setExtremes(xExtremes.min, xExtremes.max, false, false);
+
+            //Re-draw chart
+            chart.redraw();
+        }
+
+        function addSeries(key) {
+            $.get(path + '/' + key + '.json').done(function (data) {
+                renderedSeries.push(data)
+            });
+        }
+
+        function deleteSeries(key) {
+            var index = findSeries(renderedSeries, key);
+            renderedSeries.splice(index, 1)
+        }
+
+        function saveSVG(url, name) {
+            $.ajax({
+                type: "POST",
+                url: url,
+                data: JSON.stringify({
+                    svg: chart.getSVG(),
+                    name: name
+                })
+            });
+        }
+    }
 });
--- view/src/javascript/lib.js
+++ view/src/javascript/lib.js
@@ -22,6 +22,41 @@ if (!Array.prototype.findIndex) {
     };
 }
 
+if (!Array.prototype.filter) {
+    Array.prototype.filter = function(fun/*, thisArg*/) {
+        'use strict';
+
+        if (this === void 0 || this === null) {
+            throw new TypeError();
+        }
+
+        var t = Object(this);
+        var len = t.length >>> 0;
+        if (typeof fun !== 'function') {
+            throw new TypeError();
+        }
+
+        var res = [];
+        var thisArg = arguments.length >= 2 ? arguments[1] : void 0;
+        for (var i = 0; i < len; i++) {
+            if (i in t) {
+                var val = t[i];
+
+                // NOTE: Technically this should Object.defineProperty at
+                //       the next index, as push can be affected by
+                //       properties on Object.prototype and Array.prototype.
+                //       But that method's new, and collisions should be
+                //       rare, so use the more-compatible alternative.
+                if (fun.call(thisArg, val, i, t)) {
+                    res.push(val);
+                }
+            }
+        }
+
+        return res;
+    };
+}
+
 function guid() {
     function s4() {
         return Math.floor((1 + Math.random()) * 0x10000)
